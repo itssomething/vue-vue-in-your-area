e61594425cc0898d9145a66fd59382b4b12b567d
manhvd <manhvd@relipasoft.com>
2020-10-07 18:01:50 +0700

    hello vue cli

---
+++ src/components/EventCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1></h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'I want to be'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/componMake autosave optional for accepts_nested_attributes_forents/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
