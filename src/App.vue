c427e2bb15f7f9a1cf50774bffb7a5d8c9ff9ac3
manhvd <manhvd@relipasoft.com>
2020-10-07 18:52:07 +0700

    temp

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link v-bind:to="{ name: 'event-list' }">List</router-link> |
+      <router-link v-bind:to="{ name: 'event-show' }">Event show</router-link> |
+      <router-link v-bind:to="{ name: 'event-create' }">Event create</router-link>
     </div>
     <router-view />
   </div>
---
+++ src/views/EventShow.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Event show</h1>
+</template>
